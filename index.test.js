ea667e2716b2f3e455ad95b6b022335f12331b44
Leonardo López <leonardo.lopez@edunext.co>
2023-08-09 11:04:44 -0400

    feat: add inputs to the test

--- index.test.js
+++ index.test.js
@@ -5,7 +5,9 @@ const path = require('path');
 test('test runs', () => {
   process.env['INPUT_EXTRA_PRIVATE_REQUIREMENTS'] = 'false';
   process.env['INPUT_THEME_REPOSITORY'] = 'false';
-  process.env['INPUT_TUTOR_VERSION'] = '16.0.2';
+  process.env['INPUT_TUTOR_VERSION'] = '15.3.3';
+  process.env['INPUT_TUTOR_PLUGIN_SOURCES'] = '("git+https://github.com/overhangio/tutor-mfe.git" "git+https://github.com/Pearson-Advance/tutor-ecommerce" "git+https://github.com/Pearson-Advance/tutor-discovery")';
+  process.env['INPUT_TUTOR_PEARSON_PLUGIN_URL'] = 'git+https://GH_TOKEN@github.com/Pearson-Advance/tutor-pearson-plugin.git@vue/PADV-329';
   const ip = path.join(__dirname, 'index.js');
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
   console.log(result);
