3f6383aacdf32b5448f6205d3be4427a8bb73ee3
nicolas-mateoli <nicolas.mateoli@edunext.co>
2023-09-12 11:15:07 -0300

    feat: Added functions prepareEnvironment, execInVenv, installTutorPlugins, getTutorRoot, installThemes, gitClone, handlePrivatePackages. Improved existing code to match naming conventions.
    refactor: updated use example in README.md
    
    refactor: updated the way 'installTutorPlugins' manages sources
    
    fix: removed test changes
    
    fix: added '-e' to 'pip install' for plugin installation.
    
    refactor: removed config saving after installing plugins as it should not be neccesary
    
    enhancement: installTutorPlugins() can determine what type of source is installing and select the correct way of doing it
    
    enhancement: Simplify tutor package installation by removing additional dependencies.

--- index.test.js
+++ index.test.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 test('test runs', () => {
   process.env['INPUT_TUTOR_VERSION'] = '15.3.3';
-  process.env['INPUT_TUTOR_PEARSON_PLUGIN_URL'] = 'git+https://GH_TOKEN@github.com/Pearson-Advance/tutor-pearson-plugin.git@vue/PADV-329';
-  process.env['INPUT_TUTOR_PEARSON_PLUGIN_NAME'] = '("pearson-plugin-mfe-stg" "pearson-plugin-edxapp-stg" "pearson-plugin-discovery-stg" "pearson-plugin-ecommerce-stg")';
-  process.env['INPUT_TUTOR_PLUGIN_SOURCES'] = '("git+https://github.com/Pearson-Advance/tutor-ecommerce" "git+https://github.com/Pearson-Advance/tutor-discovery")';
-  process.env['INPUT_PLUGIN_NAMES'] = '("mfe" "ecommerce" "discovery")';
+  process.env['INPUT_TUTOR_PEARSON_PLUGIN_BRANCH'] = 'main';
+  process.env['INPUT_TUTOR_ADDITIONAL_PLUGIN_SOURCES'] = '("git+https://github.com/overhangio/tutor-mfe.git" "git+https://github.com/Pearson-Advance/tutor-ecommerce.git" "git+https://github.com/Pearson-Advance/tutor-discovery.git")';
+  process.env['INPUT_TUTOR_PEARSON_PLUGINS_TO_ENABLE'] = '("pearson-plugin-mfe-stg" "pearson-plugin-edxapp-stg" "pearson-plugin-discovery-stg" "pearson-plugin-ecommerce-stg")';
+  process.env['INPUT_TUTOR_PLUGINS_TO_ENABLE'] = '("mfe" "ecommerce" "discovery")';
   process.env['INPUT_EXTRA_PRIVATE_REQUIREMENTS'] = 'true';
   process.env['INPUT_PRIVATE_REPOSITORIES'] = '("pearson-core" "course_operations" "secure-cloudfront-video")';
-  process.env['INPUT_BRANCHES'] = '("master" "master" "master")';
+  process.env['INPUT_PRIVATE_REPOSITORIES_BRANCHES'] = '("master" "master" "master")';
   process.env['INPUT_THEME_REPOSITORY'] = 'https://github.com/Pearson-Advance/openedx-themes.git';
   process.env['INPUT_THEME_BRANCH'] = 'pearson-release/olive.stage';
 
